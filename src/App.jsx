d6b95f154d354c690885b4f3fa669d6a99484afe
boreallayerco-cpu <boreallayer.co@gmail.com>
2026-08-23 13:56:28 -0600

    Start SpoolStack React app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main>
+      <h1>SpoolStack</h1>
+      <p>A workspace for 3D printing makers.</p>
+
+      <button>Filament</button>
+      <button>Print Jobs</button>
+      <button>Costs</button>
+    </main>
+  )
+}
+
+export default App
